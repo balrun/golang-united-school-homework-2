e7a100b886bf67e9589a550f27380fca47317727
balrun <balrundev@yandex.ru>
2022-03-02 19:14:57 +0300

    task completed

--- solution.go
+++ solution.go
@@ -21,7 +21,7 @@ const (
 func CalcSquare(sideLen float64, sidesNum Sides) float64 {
 	var square float64 = 0
 	if sidesNum == SidesTriangle {
-		square = sideLen * math.Sqrt(3) / 4
+		square = math.Pow(sideLen, 2) * math.Sqrt(3) / 4
 	} else if sidesNum == SidesSquare {
 		square = math.Pow(sideLen, 2)
 	} else if sidesNum == SidesCircle {
